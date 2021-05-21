f273e571f37c5f811f889fbfd517963705d9b539
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-05-21 20:54:12 +0530

    fix: added release assets

--- release.config.js
+++ release.config.js
@@ -4,6 +4,11 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
+        ["@semantic-release/github", {
+            assets: [{
+                path: "./client/build.zip",
+                label: "Build"
+            }]
+        }]
     ]
 }
