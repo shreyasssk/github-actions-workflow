6501ed5679bcc9de791ceff5a000ebaa9ff5d334
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-05-21 18:53:05 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# github-actions-workflow
+# A Simple Stategy to run a clean GitHub workflows in a devops way
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   constructor() {}
+
+  isProd = environment.production;
 
   ngOnInit() {
     this.loadJsFile('assets/js/scribbler.js');
