7fe3aed5e273cce3f446b18ca641d23b5edf25a5
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-05-21 15:12:24 +0530

    Initial commit

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    this.loadJsFile('assets/js/scribbler.js');
+  }
+  public loadJsFile(url: any) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
+}
