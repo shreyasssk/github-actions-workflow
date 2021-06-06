b22de87fe5d11bec6fbee2c87096f85c1a72b5c2
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-06-06 19:28:33 +0530

    feat: Added NL Bot

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  discordLink:
+    'https://discord.com/api/oauth2/authorize?client_id=850595213122338827&permissions=415808&scope=bot',
 };
